b540ad3a3fbaa87b1f2229db716f931fd77ffeb6
Kerbusch <daankerbusch@gmail.com>
2025-11-27 15:43:32 +0100

    added week b3 and moved b1

---
+++ test.py
@@ -0,0 +1,2 @@
+import pandas
+df = pandas.DataFrame({'Mixing time': [1, 2], 'Mixing speed': [3, 4], "Viscosity" : [5, 6], "Arm length": [7, 8], "Mixing quantity": [9, 10], "liquid volume": [11, 12]})
